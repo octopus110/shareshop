14146d4b104855d1247c9c3454599872e378b672
lzl <1169815992@qq.com>
2017-12-12 11:13:58 +0800

    '关注'

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Member;
 use EasyWeChat\Foundation\Application;
 use EasyWeChat\Message\Transfer;
-use App\Http\Controllers\Controller;
 
 class WechatController extends Controller
 {
@@ -24,38 +22,7 @@ class WechatController extends Controller
         $server->setMessageHandler(function ($message) use ($user) {
             switch ($message->MsgType) {
                 case 'event':
-                    $userOpenid = $message->FromUserName;
-
-                    switch ($message->Event) {
-                        case 'subscribe':
-                            $member = new Member();
-
-                            $userInfo = $member->where('openid', $userOpenid)->select('id')->first();
-
-                            if (!$userInfo) {
-                                $id = $member->insertGetId([
-                                    'openid' => $userOpenid,
-                                    'nickname' => $user->get($userOpenid)->nickname,
-                                    'head' => $user->get($userOpenid)->headimgurl,
-                                ]);
-
-                                if ($id) {
-                                    session()->put('mid', $id);
-
-                                    return '欢迎您的到来:' . $user->get($userOpenid)->nickname;
-                                } else {
-                                    return '您的信息由于某种原因没有保存，您处于未登录状态';
-                                }
-                            } else {
-                                session()->put('mid', $userInfo->id);
-
-                                return '欢迎您的再次光临';
-                            }
-                            break;
-                        default:
-                            return '谢谢关注';
-                            break;
-                    }
+                    return '欢迎您的到来:' . $user->get($message->FromUserName)->nickname;
                     break;
                 case 'text':
                     return new Transfer();
