06d2bdb9f2bbe65a5389a16631633779af1ec79a
lzl <1169815992@qq.com>
2017-12-13 16:38:02 +0800

    'type'

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -10,7 +10,7 @@
         <div class="left">
             <p>{{ $member['nickname'] }}</p>
 
-            <p>{{ $type==1?'个体商':'普通会员' }}</p>
+            <p>{{ $member['type']==1?'个体商':'普通会员' }}</p>
         </div>
         <div class="clear"></div>
     </section>
