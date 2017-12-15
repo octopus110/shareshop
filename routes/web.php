7191d6e971bf8cd9693b7072eeb28a8c54902818
lzl <1169815992@qq.com>
2017-12-15 21:01:06 +0800

    'share'

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 Route::get('/', 'indexController@index');
 Route::get('/list/{id?}/{k?}', 'indexController@_list');
 Route::any('/ajax_list/{id?}/{k?}', 'indexController@ajax_list');//ajax获取产品 下拉刷新
-
+Route::any('/details/{id?}', 'indexController@detail');
 Route::any('/cart/deal/{id?}', 'memberController@cartDel');
 Route::any('/order/del/{id?}', 'memberController@order_del');//删除订单
 Route::any('/obligation/{type?}', 'memberController@obligation');//待付款 待签收 待发货
@@ -14,7 +14,6 @@ Route::any('/address/edit/{id?}', 'memberController@edit');
 
 Route::any('/wechat', 'WechatController@serve');
 Route::group(['middleware' => 'wechat.oauth'], function () {
-    Route::any('/details/{id?}', 'indexController@detail');
     Route::any('/cart', 'memberController@carts');//购物车
     Route::get('/member', 'memberController@member'); //用户页面
     Route::any('/create_order', 'indexController@create_order'); //创建订单
