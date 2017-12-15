091818f4903860224d160dfcacdf0be982710466
lzl <1169815992@qq.com>
2017-12-15 21:33:17 +0800

    '2354'

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -313,7 +313,7 @@
             title: '推荐给你一个好东西：{{ $data->name }}',
             desc: '{!! $data->introduce !!}',
             link: '{{ url("/details/$data->id") }}',
-            imgUrl: '{!! url("uploads/$images[0]['src']") !!}',
+            imgUrl: '{!! url("uploads/$images[0]->src") !!}',
             success: function () {
                 alert('分享成功,有人购买后讲获得奖金');
             },
@@ -327,7 +327,7 @@
         wx.onMenuShareTimeline({//朋友圈
             title: '{{ $data->name }}',
             link: '{{ url("/details/$data->id") }}',
-            imgUrl: '{!! url("uploads/$images[0]['src']") !!}',
+            imgUrl: '{!! url("uploads/$images[0]->src") !!}',
             success: function () {
                 alert('分享成功,有人购买后讲获得奖金');
             },
