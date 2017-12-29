60ee166667c43aecdb26f0f6d3dbce30be903748
lzl <1169815992@qq.com>
2017-12-29 10:26:36 +0800

    '12'

--- resources/views/pay_success.blade.php
+++ resources/views/pay_success.blade.php
@@ -37,7 +37,7 @@
 <h2>
     <i class="iconfont icon-ziyuan"></i>
 
-    <p>{{$title?$title:'支付成功'}}</p>
+    <p>{{isset($title)?$title:'支付成功'}}</p>
 </h2>
 <a href="/member">个人中心</a>
 <a href="/">返回首页</a>
