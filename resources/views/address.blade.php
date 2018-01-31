40453752078469adc3fa65b5f592cc7bef999cfc
lzl <1169815992@qq.com>
2018-01-31 14:03:28 +0800

    '12'

--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -39,9 +39,9 @@
 <div class="turnoff" style="display: none">
     <div style="width: 100vw; height: 14vw;"></div>
     <div data-toggle="distpicker">
-        <select id="province" data-province="---- 选择省 ----"></select>
-        <select id="city" data-city="---- 选择市 ----"></select>
-        <select id="district" data-district="---- 选择区 ----"></select>
+        <select id="province"></select>
+        <select id="city"></select>
+        <select id="district"></select>
     </div>
 
     <input type="text" id="address" placeholder="输入详细地址"/>
@@ -51,7 +51,7 @@
     <input type="button" value="取消" id="address_quit"/>
 </div>
 <script>
-    $("#distpicker").distpicker();
+
     $('.address-add').click(function () {
         $('.turnoff').fadeIn();
         return false;
