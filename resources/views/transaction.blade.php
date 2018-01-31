b5025292ceefc04f2f33f3b3530caf2808785244
lzl <1169815992@qq.com>
2018-01-31 12:37:24 +0800

    '12'

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -33,14 +33,14 @@
         <a href="details/{{ $item->id }}/{{ $mid }}">
             【{{ $item->type?'提现':'购物' }}】{{ $item->name }} <span>{{ $item->money }}</span>
         </a>
-        <p class="pro_code" data-id="{{ url('details/'.$item->id.'/'.$mid) }}" onclick=getCode(this)>点击获取产品二维码</p>
+        <p class="pro_code" data-id="{{ url('details/'.$item->id.'/'.$mid) }}" onclick=getCodeEr(this)>点击获取产品二维码</p>
         <img alt="二维码" title="二维码"/>
     </section>
 @endforeach
 
 <script type="javascript">
     $(function(){
-        function getCode(that) {
+        function getCodeEr(that) {
             new QRious({
                 element: $(that).next()[0],
                 value: $(that).attr('data-id'),
