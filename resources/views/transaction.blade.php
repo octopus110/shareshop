dc33f93d00703b19e27eb427642f52f1984a9b84
lzl <1169815992@qq.com>
2018-01-31 12:30:45 +0800

    '12'

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -44,7 +44,7 @@
         <a href="details/{{ $item->id }}/{{ $mid }}">
             【{{ $item->type?'提现':'购物' }}】{{ $item->name }} <span>{{ $item->money }}</span>
         </a>
-        <p class="pro_code" onclick=getCode("{{ url('details/'.$item->id.'/'.$mid) }}",this)>点击获取产品二维码</p>
+        <p class="pro_code" onclick="getCode({{ url('details/'.$item->id.'/'.$mid) }},this)">点击获取产品二维码</p>
         <img alt="二维码" title="二维码"/>
     </section>
 @endforeach
