7fddc22ef5cdfe2917f8436e6e2e61f5896a85ef
lzl <1169815992@qq.com>
2018-02-01 13:09:54 +0800

    '12'

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Member;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class homeController extends Controller
 {
