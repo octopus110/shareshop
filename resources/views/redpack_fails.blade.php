9e37a8a2f8ec5ec91ced5aa14835ffc37658a98b
lzl <1169815992@qq.com>
2018-02-02 15:21:38 +0800

    '12'

--- resources/views/redpack_fails.blade.php
+++ resources/views/redpack_fails.blade.php
@@ -9,36 +9,40 @@
     <link rel="stylesheet" href="/css/base.css">
     <link rel="stylesheet" href="/css/iconfont.css">
     <style>
-        body {
-            text-align: center;
-        }
+    body {
+        text-align: center;
+    }
 
-        .icon-fail {
-            font-size: 50vw;
-        }
+    .icon-fail {
+        font-size: 50vw;
+    }
 
-        h2 p {
-            font-size: 10vw;
-            color: red;
-        }
+    h2 p {
+        font-size: 10vw;
+        color: red;
+    }
 
-        a {
-            display: block;
-            width: 70vw;
-            padding: 3vw 0;
-            margin: 6vw auto;
-            background: #fff;
-            border-radius: 3vw;
-        }
-    </style>
+    a {
+        display: block;
+        width: 70vw;
+        padding: 3vw 0;
+        margin: 6vw auto;
+        background: #fff;
+        border-radius: 3vw;
+    }
+</style>
 </head>
 <body>
-<h2>
-    <i class="iconfont icon-fail"></i>
+    <h2>
+        <i class="iconfont icon-fail"></i>
 
-    <p>{{$title?$title:'失败'}}</p>
-</h2>
-<a href="/member">个人中心</a>
-<a href="/">返回首页</a>
+        <p>{{$title?$title:'失败'}}</p>
+    </h2>
+    @if($title == '个人信息不完整')
+        <a href="/improve">完善个人信息</a>
+    @else
+        <a href="/member">个人中心</a>
+    @endif
+    <a href="/">返回首页</a>
 </body>
 </html>
