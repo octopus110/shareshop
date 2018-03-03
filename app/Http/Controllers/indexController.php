724d092215cc3a13307198b53236bdd87411ec60
lzl <1169815992@qq.com>
2018-03-03 16:59:12 +0800

    '12'

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -39,7 +39,7 @@ class indexController extends Controller
     //测试微信支付
     public function test()
     {
-        $tools = new APP\libs\JsApiPay();
+        $tools = new \App\libs\JsApiPay();
 
         $openId = $tools->GetOpenid();
 
